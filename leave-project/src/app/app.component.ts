e4f3ceb4b7d24308ecd91080a346efdac0981673
labibi.lg <labibi.lg@gmail.com>
2022-08-25 15:03:26 +0800

    add: 基本請假功能

---
+++ leave-project/src/app/app.component.ts
@@ -0,0 +1,118 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject } from '@angular/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import * as moment from 'moment';
+import { NgxMaterialTimepickerTheme } from 'ngx-material-timepicker';
+
+
+
+interface selectModel {
+  name: string;
+  value: string;
+}
+export const MY_FORMATS = { parse: { dateInput: 'LL', }, display: { dateInput: 'LL', monthYearLabel: 'MMM YYYY', dateA11yLabel: 'LL', monthYearA11yLabel: 'MMMM YYYY', }, };
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'zh-TW' },
+    // { provide: MAT_DATE_FORMATS, useValue: TW_FORMATS }
+  ]
+})
+export class AppComponent {
+  constructor(
+    private elementRef: ElementRef
+  ) { }
+  title = 'leave-project';
+  userName: string = '';
+  reason: string = '';
+
+  reasonRadio: '0' | '1' = '0';
+
+  date = moment(new Date());
+  // startDate = moment(new Date());
+  action: '0' | '1' | '2' | '3' = '0';
+  startTime = '09:00';
+  endTime = '09:30';
+  content = '';
+
+  darkTheme: NgxMaterialTimepickerTheme = {
+    container: {
+      bodyBackgroundColor: '#424242',
+      buttonColor: '#fff'
+    },
+    dial: {
+      dialBackgroundColor: '#555',
+    },
+    clockFace: {
+      clockFaceBackgroundColor: '#555',
+      clockHandColor: '#9fbd90',
+      clockFaceTimeInactiveColor: '#fff'
+    }
+  };
+  members: selectModel[] = [
+    { name: 'EnPeiLo【羅恩培】', value: 'EnPeiLo【羅恩培】' },
+    { name: 'MisuSu【蘇亭郡】', value: 'MisuSu【蘇亭郡】' },
+    { name: 'MichelleHsieh【謝婷蓁】', value: 'MichelleHsieh【謝婷蓁】' },
+    { name: 'MattWu【吳維珉】', value: 'MattWu【吳維珉】' },
+    { name: 'VanessaHsu【許澐易】', value: 'VanessaHsu【許澐易】' },
+    { name: 'JungKang【康家榮】', value: 'JungKang【康家榮】' },
+    { name: 'ArthurYu【游超能】', value: 'ArthurYu【游超能】' },
+    { name: 'IanLau【劉昱揚】', value: 'IanLau【劉昱揚】' },
+    { name: 'LewisLin【林紀霖】', value: 'LewisLin【林紀霖】' },
+    { name: 'StephenChang【張世賢】', value: 'StephenChang【張世賢】' },
+    { name: 'XavierKuo【郭嘉元】', value: 'XavierKuo【郭嘉元】' },
+    { name: 'AllenLee【李東昇】', value: 'AllenLee【李東昇】' },
+  ];
+
+  reasons: selectModel[] = [
+    { name: '身體不適', value: '身體不適' },
+    { name: '個人事務處理', value: '個人事務處理' },
+    { name: '路上塞車', value: '路上塞車' },
+    { name: '私事待辦', value: '私事待辦' },
+    { name: '休假', value: '休假' },
+    { name: '醫院回診', value: '醫院回診' },
+    { name: '內部訓練', value: '內部訓練' },
+  ];
+
+  create(): void {
+    const tempDate = `${this.date.month() + 1}/${this.date.date()}`
+    this.content = `- 請假人員：${this.userName}\n- 請假時間：${tempDate} ${this.startTime}-${this.endTime} \n- 請假事由：${this.reason}`
+
+  }
+  actionChange(): void {
+    switch (this.action) {
+      case '0':
+        this.startTime = '09:00';
+        this.endTime = '09:30';
+        break;
+      case '1':
+        this.startTime = '09:00';
+        this.endTime = '17:30';
+        break;
+      case '2':
+        this.startTime = '09:00';
+        this.endTime = '12:00';
+        break;
+      case '3':
+        this.startTime = '13:00';
+        this.endTime = '17:30';
+        break;
+    }
+  }
+
+  copy(selector: string): void{
+    const input = this.elementRef.nativeElement.querySelector(selector);
+    input.select();
+    document.execCommand('copy');
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+}
+
---
+++ leave-project/src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+
+export const TW_FORMATS = {
+  parse: {
+    dateInput: 'MM/DD'
+  },
+  display: {
+    dateInput: 'MM/DD',
+    monthYearLabel: 'YYYY MMM',
+    dateA11yLabel: 'YYYY/MM/DD',
+    monthYearA11yLabel: 'YYYY MMM'
+  }
+};
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatFormFieldModule,
+    NgxMaterialTimepickerModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'zh-TW' },
+    { provide: MAT_DATE_FORMATS, useValue: TW_FORMATS }
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
